c85ff06736fd80543b9a514d8c5b3f707b2877d6
ryushi-masa <pomechan.0505@icloud.com>
2023-01-08 23:30:57 +0900

    pagesを読み込むためのタグを追加、pagesディレクトリ作成

--- src/nuxt-app/app.vue
+++ src/nuxt-app/app.vue
@@ -1,17 +1,3 @@
 <template>
-<v-conteiner style="margin: 1000px">
-    <!-- <Test /> -->
-    <Test1 />
-</v-conteiner>
+  <NuxtPage />
 </template>
-
-<script>
-import Test from "./Test";
-import Test1 from "./Test1";
-export default {
-  components: {
-    Test,
-    Test1,
-  }
-};
-</script>
