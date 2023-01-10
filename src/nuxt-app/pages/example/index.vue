c559b3965c1ed3e2b30c521d732549d72ecb8c5b
ryushi-masa <pomechan.0505@icloud.com>
2023-01-10 21:51:20 +0900

    Composition APIの学習 data、computed、methodsの使い方

--- src/nuxt-app/pages/example/index.vue
+++ src/nuxt-app/pages/example/index.vue
@@ -1,9 +1,25 @@
-<template>
-  <div>Example Page</div>
-</template>
-
-<script>
-definePageMeta({
-  layout: 'example-app'
-})
+<script setup>
+import { ref } from 'vue'
+  const name = ref('太郎')
+  const num1 = ref(1)
+  const num2 = ref(1)
+  const num = computed(() =>  num1.value + num2.value)
+  const increment = () => num1.value++
+  const consoleLog = () => console.log(12345)
+  const consoleLog1 = (async() => {
+    console.log("not async")
+    await console.log("async")
+    console.log("not async")
+  })
+  consoleLog1()
+  consoleLog()
 </script>
+
+<template>
+  <div>
+    Hello World
+    <div>{{ name }}</div>
+    <div>{{ num }}</div>
+    <v-btn @click="increment">足す</v-btn>
+  </div>
+</template>
--- src/nuxt-app/pages/index.vue
+++ src/nuxt-app/pages/index.vue
@@ -1,3 +1,17 @@
 <template>
-  <div>Hello World</div>
+  <div>
+    Hello World
+    <div>{{ name }}</div>
+    <div>{{ num }}</div>
+    <v-btn @click="increment">足す</v-btn>
+  </div>
 </template>
+
+<script setup>
+import { ref } from 'vue'
+  const name = ref('太郎')
+  const num1 = ref(1)
+  const num2 = ref(1)
+  const num = computed(() =>  num1.value + num2.value)
+  const increment = () => num1.value++;
+</script>
