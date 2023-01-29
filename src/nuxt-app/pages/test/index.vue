6cdc6a31d7ee41b9b7ff8fad9bb2a8b87629aec1
ryushi-masa <pomechan.0505@icloud.com>
2023-01-29 18:56:36 +0900

    動的なページタイトルにするための処理

---
+++ src/nuxt-app/pages/test/index.vue
@@ -0,0 +1,21 @@
+<script setup>
+useHead({
+  title: 'マイページ'
+})
+const { $foo, $bar, $log } = useNuxtApp()
+console.log(1)
+console.log(useNuxtApp())
+console.log($foo)
+console.log($bar)
+$log()
+// composablesTest.trumpMark = { id: 1, name: '太郎'}
+console.log(composableTest())
+console.log(composableTest.trumpMark)
+</script>
+<template>
+  <div>
+    test example
+    {{ $foo }}
+    <NuxtLink to="/test/example">To example</NuxtLink>
+  </div>
+</template>
