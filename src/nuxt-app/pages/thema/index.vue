47fbcd652ed098e4eae053d069ceae79bb31949b
ryushi-masa <pomechan.0505@icloud.com>
2023-02-16 23:44:30 +0900

    テーマを反映するためのファイルを追加

---
+++ src/nuxt-app/pages/thema/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <v-theme-provider theme="adminTheme">
+      <div class="bg-primary">
+        adminTheme
+      </div>
+      <v-card theme="userTheme">
+        <div class="bg-primary">
+          userTheme
+        </div>
+      </v-card>
+    </v-theme-provider>
+  </div>
+</template>
