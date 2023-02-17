03fa2cf8c187c546bc06da371f908a5a13fb076a
ryushi-masa <pomechan.0505@icloud.com>
2023-02-18 00:11:36 +0900

    テーマを反映 以下の方法で設定 Vuetifyのコンポーネントの配下に反映 v-theme-providerの配下に反映

--- src/nuxt-app/pages/thema/index.vue
+++ src/nuxt-app/pages/thema/index.vue
@@ -1,15 +1,23 @@
 <template>
   <div>
-    <v-theme-provider theme="adminTheme">
-      <v-card theme="adminTheme">
-        <div class="bg-primary">
-          adminTheme
-        </div>
+    <v-card theme="userTheme">
+      <div class="bg-primary">
+        userTheme
+      </div>
+    </v-card>
+    <v-theme-provider theme="userTheme">
+      <v-card class="bg-primary">
+        adminTheme
       </v-card>
-      <v-card theme="userTheme">
-        <div class="bg-primary">
-          userTheme
-        </div>
+    </v-theme-provider>
+    <v-card theme="adminTheme">
+      <div class="bg-primary">
+        adminTheme
+      </div>
+    </v-card>
+    <v-theme-provider theme="adminTheme">
+      <v-card class="bg-primary">
+        adminTheme
       </v-card>
     </v-theme-provider>
   </div>
