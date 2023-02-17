740afac40f711b8165f7a39aecad4a61083e1339
ryushi-masa <pomechan.0505@icloud.com>
2023-02-17 23:55:09 +0900

    2種類のテーマを反映

--- src/nuxt-app/pages/thema/index.vue
+++ src/nuxt-app/pages/thema/index.vue
@@ -1,9 +1,11 @@
 <template>
   <div>
     <v-theme-provider theme="adminTheme">
-      <div class="bg-primary">
-        adminTheme
-      </div>
+      <v-card theme="adminTheme">
+        <div class="bg-primary">
+          adminTheme
+        </div>
+      </v-card>
       <v-card theme="userTheme">
         <div class="bg-primary">
           userTheme
